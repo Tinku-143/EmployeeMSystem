0c2c75c85735ca946e4b6b92dd08688350651431
@Tinku-143 <tinkujaya143@gmail.com>
2025-05-10 17:52:59 +0530

    hibernate configuration files

--- EmployeeMsSystem/src/main/java/com/ems/www/config/HibernateUtil.java
+++ EmployeeMsSystem/src/main/java/com/ems/www/config/HibernateUtil.java
@@ -1,5 +1,26 @@
 package com.ems.www.config;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
-    
+	private static final SessionFactory sessionFactory;
+	
+	static {
+		try {
+			sessionFactory=new Configuration().configure().buildSessionFactory();
+		}catch(Exception ex) {
+			ex.printStackTrace();
+			throw new ExceptionInInitializerError("unable to Create the SessionFactory");
+		}
+	}
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+	public static void shutdown() {
+		if(sessionFactory!=null) {
+			sessionFactory.close();
+		}
+	}
+
 }
